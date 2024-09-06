3e6e2bfac7a244f3c840e170edc213962988018b
Kristaps <chinalover300@gmail.example>
2024-09-06 10:06:04 +0300

    Kautkas specifiska faila izvele!

---
+++ select/sql3.sql
@@ -0,0 +1,5 @@
+-- uzdvevmumiemv
+use sql_store;
+-- select * from orders where order_date > '2018-01-01 and order_date < '2019-01-01';
+
+-- select * from customers where state != "VA" and state NOT IN ('CO'); 
