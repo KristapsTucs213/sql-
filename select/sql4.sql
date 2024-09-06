63bab8416b448ed730809d5a683c2dcb09096d9f
Kristaps <chinalover300@gmail.example>
2024-09-06 10:18:27 +0300

    Kautkas pa or un and

---
+++ select/sql4.sql
@@ -0,0 +1,6 @@
+-- uzdevumiem
+
+use sql_store;
+-- select * from customers where points > 1000 and birth_date > '1990-01-01';
+-- select * from customers where points > 1000 or birth_date > '1990-01-01';
+-- select * from customers where birth_date > '1990-01-01' or points > 1000 and state = "VA" or "CO";
